17389c4b0adde08e84b13bf3a43206679369723d
Zacbemis <zac.bemis916@gmail.com>
2025-11-06 21:23:55 -0800

    remove foot

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./globals.css";
 import Navigation from "./components/navigation";
-import Footer from "./components/footer";
 import RainOverlay from "./components/rain-overlay";
 import yosemiteBackground from "./public/assets/yosemite.png";
 
@@ -20,7 +19,6 @@ export default function RootLayout({
         <div className="relative z-10 flex min-h-screen flex-col">
           <Navigation />
           <div className="flex-1">{children}</div>
-          <Footer />
         </div>
       </body>
     </html>
